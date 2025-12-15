4385ce13dbc708f466ef7712de3ad4fab8fe7520
yoshoku <yoshoku@outlook.com>
2025-12-16 08:40:36 +0900

    chore: add eslint and prettier configuration files

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+import vueParser from 'vue-eslint-parser'
+import prettierConfig from '@vue/eslint-config-prettier'
+import globals from 'globals'
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  prettierConfig,
+
+  {
+    files: ['**/*.{js,mjs,cjs,ts,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'vue/multi-word-component-names': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+
+  {
+    files: ['*.config.{js,ts,mjs,cjs}', 'vite.config.ts', '**/*.cjs'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+
+  {
+    ignores: ['dist/**', 'node_modules/**', '.husky/**', 'public/**', '*.min.js'],
+  },
+]
