47070fbe40ae7601b10eea9d661e3e5ff71afd6d
yoshoku <yoshoku@outlook.com>
2025-12-16 15:06:21 +0900

    chore: remove unnecessary worker

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { createApp } from 'vue'
 import * as monaco from 'monaco-editor'
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
 import { loader } from '@guolao/vue-monaco-editor'
 import './style.css'
@@ -11,15 +8,6 @@ import App from './App.vue'
 
 self.MonacoEnvironment = {
   getWorker(_: unknown, label: string) {
-    if (label === 'json') {
-      return new jsonWorker()
-    }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return new cssWorker()
-    }
-    if (label === 'html' || label === 'headlebars' || label === 'razor') {
-      return new htmlWorker()
-    }
     if (label === 'typescript' || label === 'javascript') {
       return new tsWorker()
     }
